8be825d0cf9e4ee04e6bf6aeac3d1af5841bbb6a
BR <snkp002@gmail.com>
2025-10-18 16:18:49 +0800

    Initial Commit

---
+++ Sources/BRMacrosPlugin/BRMacrosPlugin.swift
@@ -0,0 +1,16 @@
+//
+//  BRMacrosPlugin.swift
+//  BRMacrosPlugin
+//
+
+import SwiftCompilerPlugin
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+
+@main
+struct BRMacrosPlugin: CompilerPlugin {
+    let providingMacros: [Macro.Type] = [
+    ]
+}
