de81cdfd4dcbe157c70899b033018f3df33cac74
BR <snkp002@gmail.com>
2025-10-26 14:48:44 +0800

    feat: 新增 @_exported import OSLog to BRMacros

--- Sources/BRMacros/BRMacros.swift
+++ Sources/BRMacros/BRMacros.swift
@@ -1,5 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+@_exported import OSLog
+
 @freestanding(expression)
 public macro BRLog(_ category: BRLogCategory, _ level: BRLogLevel, _ message: String) = #externalMacro(module: "BRMacrosPlugin", type: "LogMacro")
